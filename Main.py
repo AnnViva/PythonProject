ed6e0ba98caa00e2b59823b6ac4f1c8dff84afc3
Денис <denisnovikov94@gmail.com>
2018-11-20 17:17:28 +0300

    Добавлены запросы к АПИ Телеграма

--- Main.py
+++ Main.py
@@ -1,4 +1,28 @@
-def main():
-    print("Hello World")
+import requests
 
-main()
+url = "https://api.telegram.org/bot795876225:AAEL9d7-ormWBa7SbyJNRriK_UdBe7Y494E/"
+
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+send_mess(chat_id, 'Your message goes here')
